92e53f2219489f3154988c8c039124424c975d28
Danielle Dookie <danielledookie90@gmail.com>
2018-10-22 01:05:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
+document.querySelector ('#main').remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = 'Danielle is the Champion!'
+document.body.appendChild(header)
+const newHeader = document.querySelector('h1#victory');
+
+
